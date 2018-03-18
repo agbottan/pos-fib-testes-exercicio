edd23e1b10eacf061248b8c4edcd6dfb3afd9093
André G. Bottan <agbottan@gmail.com>
2018-03-17 22:20:59 -0300

    Separa do Banco

--- src/test/java/br/fib/tdd/salarioTest/SalarioCalc.java
+++ src/test/java/br/fib/tdd/salarioTest/SalarioCalc.java
@@ -6,6 +6,7 @@ import br.fib.tdd.salario.Salario;
 
 public class SalarioCalc {
 
+<<<<<<< HEAD
 	public static void calcula(int horas, double valor, int dep) {
 
 		Salario sal = new Salario(horas, valor, dep);
@@ -14,11 +15,26 @@ public class SalarioCalc {
 		System.out.println("INSS\t" 	+ sal.calculaDescontoINSS());
 		System.out.println("IR\t" 	   	+ sal.calculaDescontoIR());
 		System.out.println("Líquido\t"	+ sal.calculaSalarioLiquido());
+=======
+	public static void calcula(String titulo, int horas, double valor, int dep) {
+
+		Salario sal = new Salario(horas, valor, dep);
+
+		System.out.println("------------------------");
+		System.out.println(titulo);
+		System.out.println("------------------------");
+		System.out.println("Bruto\t"   	+ sal.calculaSalarioBruto());
+		System.out.println("INSS\t" 	+ sal.calculaDescontoINSS());
+		System.out.println("IR\t" 		+ sal.calculaDescontoIR());
+		System.out.println("Líquido\t"	+ sal.calculaSalarioLiquido());
+		System.out.print("\n");
+>>>>>>> Calcula
 	}
 
 	public static void main(String[] args){
 
 		// Caso 1
+<<<<<<< HEAD
 		calcula(80, 20, 2);
 
 		// Caso 2
@@ -26,5 +42,17 @@ public class SalarioCalc {
 		// Caso 3
 
 		// Caso 4
+=======
+		calcula("CASO 1", 80, 20, 2);
+
+		// Caso 2
+		calcula("CASO 2", 80, 10, 3);
+
+		// Caso 3
+		calcula("CASO 3", 60, 15, 1);
+
+		// Caso 4
+		calcula("CASO 4", 60, 100, 4);
+>>>>>>> Calcula
 	}
 }
--- src/test/java/br/fib/tdd/salarioTest/SalarioTest.java
+++ src/test/java/br/fib/tdd/salarioTest/SalarioTest.java
@@ -2,23 +2,39 @@ package br.fib.tdd.salarioTest;
 
 // Externos
 import static org.junit.Assert.assertEquals;
+<<<<<<< HEAD
+=======
+
+import org.junit.Before;
+>>>>>>> Calcula
 import org.junit.Test;
 
 // Internos
 import br.fib.tdd.salario.Salario;
 
 /*
+<<<<<<< HEAD
 -------------------------------
 	Caso 1 -> Sem descontos
 -------------------------------
+=======
+-------------------------
+	Caso 1
+-------------------------
+>>>>>>> Calcula
 
 80 horas
 $20 / hora
 2 dependentes
 
 Bruto:   $ 1700,00
+<<<<<<< HEAD
 INSS:	 $ ??,00
 IR:		 $ ??,00
+=======
+INSS:	 $ 153,00
+IR:		 $ 119,00
+>>>>>>> Calcula
 Liquido: $ 1428,00
 
 
@@ -26,6 +42,7 @@ Liquido: $ 1428,00
 	Caso 2
 -------------------------
 
+<<<<<<< HEAD
 60 horas
 $40 / hora
 4 dependentes
@@ -33,6 +50,16 @@ $40 / hora
 Bruto:   $ ??,00
 
 Liquido: $ ??,00
+=======
+80 horas
+$10 / hora
+3 dependentes
+
+Bruto:	 $ 950,00
+INSS:	 $ 80,75
+IR:		 $ 47,50
+Líquido: $ 821,75
+>>>>>>> Calcula
 
 
 -------------------------
@@ -40,6 +67,7 @@ Liquido: $ ??,00
 -------------------------
 
 60 horas
+<<<<<<< HEAD
 $40 / hora
 4 dependentes
 
@@ -53,10 +81,35 @@ calculaSalarioLiquido
 calculaSalarioBruto
 calculaDescontoIR
 calculaDescontoINSS
+=======
+$15 / hora
+1 dependente
+
+Bruto:	 $ 950,00
+INSS:	 $ 80,75
+IR:		 $ 47,50
+Líquido: $ 821,75
+
+
+-------------------------
+	Caso 4
+-------------------------
+
+60 horas
+$100 / hora
+4 dependente
+
+Bruto:	 $ 6200,00
+INSS:	 $ 558,00
+IR:		 $ 434,00
+Líquido: $ 5208,00
+
+>>>>>>> Calcula
 */
 
 public class SalarioTest {
 	
+<<<<<<< HEAD
 	Salario salario = new Salario(80, 20, 2);
 	
 	@Test
@@ -65,4 +118,59 @@ public class SalarioTest {
 		assertEquals(1428, salario.calculaSalarioBruto(), 0.00001);
 	}
 
+=======
+	private Salario salario1;
+	private Salario salario2;
+	private Salario salario3;
+	private Salario salario4;
+
+	@Before
+    public void setUpTestes() {
+    	/* Salario( horas mês, $ por hora, dependentes) */
+		salario1 = new Salario(80, 20,  2);
+		salario2 = new Salario(80, 10,  3);
+		salario3 = new Salario(60, 15,  1);
+		salario4 = new Salario(60, 100, 4);
+    }
+	
+	// Teste para o método 'Salario.calculaSalarioBruto()'
+	@Test
+	public void deveCalcularSalarioBruto(){
+
+		assertEquals( 1700.00, salario1.calculaSalarioBruto(), 0.00001);
+		assertEquals(  950.00, salario2.calculaSalarioBruto(), 0.00001);
+		assertEquals(  950.00, salario3.calculaSalarioBruto(), 0.00001);
+		assertEquals( 6200.00, salario4.calculaSalarioBruto(), 0.00001);
+	}
+
+	// Teste para o método 'Salario.calculaDescontoINSS()'
+	@Test
+	public void deveCalcularDescontoINSS(){
+
+		assertEquals( 153.00, salario1.calculaDescontoINSS(), 0.00001);
+		assertEquals(  80.75, salario2.calculaDescontoINSS(), 0.00001);
+		assertEquals(  80.75, salario3.calculaDescontoINSS(), 0.00001);
+		assertEquals( 558.00, salario4.calculaDescontoINSS(), 0.00001);
+	}
+
+	// Teste para o método 'Salario.calculaDescontoIR()'
+	@Test
+	public void deveCalcularDescontoIR(){
+
+		assertEquals( 119.00, salario1.calculaDescontoIR(), 0.00001);
+		assertEquals(  47.50, salario2.calculaDescontoIR(), 0.00001);
+		assertEquals(  47.50, salario3.calculaDescontoIR(), 0.00001);
+		assertEquals( 434.00, salario4.calculaDescontoIR(), 0.00001);
+	}
+
+	// Teste para o método 'Salario.calculaSalarioLiquido()'
+	@Test
+	public void deveCalcularSalarioLiquido(){
+
+		assertEquals( 1428.00, salario1.calculaSalarioLiquido(), 0.00001);
+		assertEquals(  821.75, salario2.calculaSalarioLiquido(), 0.00001);
+		assertEquals(  821.75, salario3.calculaSalarioLiquido(), 0.00001);
+		assertEquals( 5208.00, salario4.calculaSalarioLiquido(), 0.00001);
+	}
+>>>>>>> Calcula
 }
